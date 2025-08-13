db8dca2238a2363e83efacaaf2e00a87471f8ca4
Joodi <winbeta.com@gmail.com>
2025-08-13 20:51:59 +0330

    up

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
             <div className="flex">
                 <Sidebar />
                 <div className="flex flex-col w-full">
-                    <div className="flex items-center justify-center w-full py-3 max-h-[60px] px-12 border border-b">
+                    <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border border-b">
                         <h3>Admin Panel</h3>
                         <Image src={assets.profile_icon} width={40} alt="" />
                     </div>
