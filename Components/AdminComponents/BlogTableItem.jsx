706c6494d62a6cd87ab23261339b09abf7541d40
Joodi <winbeta.com@gmail.com>
2025-08-14 17:01:27 +0330

    up

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,30 @@
+import { assets } from "@/Assets/assets";
+import Image from "next/image";
+
+const BlogTableItem = ({ authorImg, title, author, date }) => {
+
+    const BlogDate = new Date(date);
+
+    console.log("date", date)
+    console.log("BlogDate", BlogDate)
+
+    return (
+        <tr className="bg-white border-b">
+            <th scope="row" className="items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <Image src={authorImg ? authorImg : assets.profile_icon} className="rounded-full border border-gray-300" width={40} height={40} alt="" />
+                <p>{author ? author : "No author"}</p>
+            </th>
+            <td className="px-6 py-4">
+                {title ? title : "No title"}
+            </td>
+            <td className="px-6 py-4">
+                {BlogDate.toDateString()}
+            </td>
+            <td className="px-6 py-4 cursor-pointer">
+                x
+            </td>
+        </tr>
+    );
+}
+
+export default BlogTableItem;
